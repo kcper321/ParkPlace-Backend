05179745db5cdc78bd7db1e1180e3ee5ef90bd49
kacper-pawski <kacper.pawski@globallogic.com>
2025-02-19 12:27:52 +0100

    Add project files.

---
+++ Controllers/ParkingController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkingAPI.Data;
+using ParkingAPI.Models;
+
+namespace ParkingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParkingController : ControllerBase
+    {
+        private readonly ParkingContext _context;
+
+        public ParkingController(ParkingContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("available-spots")]
+        public async Task<ActionResult<IEnumerable<ParkingSpot>>> GetAvailableParkingSpots()
+        {
+            var availableSpots = await _context.ParkingSpots
+                .Where(p => p.AvailableSpots > 0)
+                    .ToListAsync();
+
+            return Ok(availableSpots);
+        }
+    }
+}
---
+++ Models/ParkingSpot.cs
@@ -0,0 +1,10 @@
+﻿namespace ParkingAPI.Models
+{
+    public class ParkingSpot
+    {
+        public int Id { get; set; }
+        public int ParkingLotId { get; set; }
+        public string Location { get; set; }
+        public int AvailableSpots { get; set; }
+    }
+}
