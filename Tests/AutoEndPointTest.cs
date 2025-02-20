98daf87eda1b099ff08de8cb1cf753936b67654b
kacper-pawski <kacper.pawski@globallogic.com>
2025-02-20 13:58:37 +0100

    Add files via upload

---
+++ Tests/AutoEndPointTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace ParkingAPI.Tests
+{
+    public class AutoEndPointTest
+    {
+        public async Task TestEndpoint_ParkingSpots()
+        {
+            var app = new WebApplicationFactory<Program>();
+            var client = app.CreateClient();
+            var response = await client.GetAsync("/api/parking/available-spots");
+            Assert.Equal(200, (int)response.StatusCode);
+        }
+    }
+}
