9b5a34732954a42bec0fca108ff9f82909cc4986
kacper-pawski <kacper.pawski@globallogic.com>
2025-02-28 14:12:30 +0100

    reserv endpoint

---
+++ ParkingAPI/Models/ParkingSpot.cs
@@ -0,0 +1,24 @@
+﻿namespace ParkingAPI.Models
+{
+    public class ParkingSpot
+    {
+        public int Id { get; set; }
+        public int Floor { get; set; }
+        public string? Zone { get; set; }
+        public int Number { get; set; }
+
+        public List<Reservation>? Reservations { get; set; }
+    }
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ParkingSpotId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool Day { get; set; }
+        public bool Status { get; set; }
+
+        public ParkingSpot? ParkingSpot { get; set; }
+    }
+}
