e43a648f63ad7a1f347a9ed405a5c1486e070eb6
kcper321 <kacper.giza@globallogic.com>
2025-03-13 15:12:00 +0100

    Update Program.cs

--- ParkingAPI/Program.cs
+++ ParkingAPI/Program.cs
@@ -9,8 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ParkingContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDbContext<DatabaseContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 
 
